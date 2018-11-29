9ebf9fd5ba339c46705f031a7d84d6423b665a3f
luolianxi <cpu887@163.com>
2018-11-29 16:47:50 +0800

    Site updated: 2018-11-29 16:47:50

--- js/SimpleCore.js
+++ js/SimpleCore.js
@@ -146,8 +146,8 @@ var SimpleCore = {
             $('body').removeClass('night-mode');
             btn.find('i').attr('class', 'fa fa-sun-o');
             $(".cover-img").css({
-                'background': "url('/images/bg1.png')",
-                'background-image': '/images/bg1.png',
+                'background': "url('/images/cover-day.jpg')",
+                'background-image': '/images/cover-day.jpg',
                 'background-size': 'cover',
                 'background-position': 'center',
                 'background-repeat': 'no-repeat'
